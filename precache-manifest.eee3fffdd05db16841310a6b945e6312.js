9273fef8d58b0578a86e0b9dcd41973296bd49a7
taiyounari@gmail.com <taiyounari@gmail.com>
2022-06-09 19:07:21 +0700

    Updates

--- precache-manifest.eee3fffdd05db16841310a6b945e6312.js
+++ precache-manifest.eee3fffdd05db16841310a6b945e6312.js
@@ -88,16 +88,16 @@ self.__precacheManifest = [
     "url": "/operationSin/static/media/cardBox.e18f5b9d.png"
   },
   {
+    "revision": "b1b2e2db2eef02d230247a474d1d66a6",
+    "url": "/operationSin/static/media/chiller.b1b2e2db.ttf"
+  },
+  {
     "revision": "34f9dff90ce072a48379d852bab181c0",
     "url": "/operationSin/static/media/forever.34f9dff9.otf"
   },
   {
     "revision": "9c2a5a0ad1ada8b84e3a37c16ed12343",
     "url": "/operationSin/static/media/franken.9c2a5a0a.otf"
-  },
-  {
-    "revision": "b1b2e2db2eef02d230247a474d1d66a6",
-    "url": "/operationSin/static/media/chiller.b1b2e2db.ttf"
   },
   {
     "revision": "b3dafea31752a3c3d3bcb06c177f8de6",
